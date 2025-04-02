05e17077480ffd95ab5f294effdf0550215f9c0b
EarthSword28 <jorden.van.nunen@gmail.com>
2025-04-02 15:29:37 +0200

    Main Page V1

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 BASE_URL = "https://my-json-server.typicode.com/EarthSword28/KdG-IoT-opdracht-Json_API-webserver"
+TYPES_URL = "https://my-json-server.typicode.com/EarthSword28/KdG-IoT-opdracht-Json_API-webserver/Types"
+SUB_TYPES_URL = "https://my-json-server.typicode.com/EarthSword28/KdG-IoT-opdracht-Json_API-webserver/Sub-Types"
 
-r = requests.get((BASE_URL+"/Types"), params={"Type": "X"})
+@app.route("/")
+def serverMainPage():
+  response = requests.get(TYPES_URL)
+  response_dict = response.json()
+  list_type = []
+  for dictionary in response_dict:
+    list_type.append(dictionary["Type"])
+  return render_template("mainPage.html", type_list=list_type)
 
-dictionary = r.json()
-
-x = dictionary[0]["Type"]
-
-print(r.url)
+app.run(host='0.0.0.0', port=5000)      # http://localhost:5000
